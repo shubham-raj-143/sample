dda4877e5ee0f6b632c28d810a6d517de4b098ec
webandandroiddevelopment <68722673+webandandroiddevelopment@users.noreply.github.com>
2020-07-24 09:46:15 +0530

    Add files via upload

---
+++ CoronaMap/index.js
@@ -0,0 +1,32 @@
+function updateMap() {
+    console.log("Updating maap with realtime data");
+    
+    fetch("/data.json")
+    .then(response => response.json())
+    .then(rsp => {
+        // console.log(rsp.data)
+        rsp.data.forEach(element => {
+            latitude= element.latitude;
+            longitude= element.longitude;
+
+            cases= element.infected;
+        if (cases > 255){
+            color ="rgb(255, 0, 0)";
+        }
+        else{
+            color= `rgb(${cases}, 0, 0)`
+        }
+            //Mark on the map
+            new mapboxgl.Marker({
+                draggable: false,
+                color: color
+            })
+                .setLngLat([longitude, latitude])
+                .addTo(map);
+            
+        });
+    })
+    
+}
+let interval = 2000;
+setInterval(updateMap, interval);
